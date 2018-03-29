c4b78462a2cc6792f124ff527bdd5dfca03d2d25
雷逸志 <ray8863@leiyizhide-MacBook-Pro.local>
2018-03-30 00:17:53 +0800

    Click on li: 1. Remove it from ul. 2. Add it to history list.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const utils = require('./utils/index.js');
 //     return element;
 // }
 
+// DOM methods
 var wrapContainer = (innerNode, wrapperEle = 'div') => {
   const constainer = document.createElement(wrapperEle);
   constainer.id = 'input-container';
@@ -26,9 +27,132 @@ var wrapContainer = (innerNode, wrapperEle = 'div') => {
   constainer.appendChild(innerNode);
 }
 
+var replaceWith = (replacedNode, newNode) => {
+  replacedNode.parentNode.insertBefore(newNode, replacedNode);
+  replacedNode.parentNode.removeChild(replacedNode);
+}
+
 var handler = (e) => {
-  // Get data first
+  // Data initializing
   this.data = {};
+  this.historyList = [];
+
+  var _this = this;
+
+  // Handler Initializing
+  var logoNameHandler = (selectedItem, selectedLi, displayContainer) => (e) => {
+    selectedItem.isInHistory = true;
+    this.historyList.push(selectedItem);
+    console.log('historyList', this.historyList);
+
+    addItemToHistory(selectedLi, displayContainer);
+  }
+
+  // UI render methods
+  // Create basic li children
+  var getLogoNameWrapper = (item, li, displayContainer) => {
+    // Create first element, img
+    let appImg = document.createElement('img');
+    appImg.src = item.logo;
+    appImg.classList.add('app-img');
+
+    // Create second element, p
+    let appNameWrapper = document.createElement('p');
+    let appNameText = document.createTextNode(`${item.name}`);
+    appNameWrapper.classList.add('app-name');
+    appNameWrapper.appendChild(appNameText);
+
+    // Create wrapper for logo & name
+    let logoNameWrapper = document.createElement('div');
+    logoNameWrapper.classList.add('logo-name-wrapper');
+    logoNameWrapper.insertBefore(appImg, logoNameWrapper.firstChild);
+    logoNameWrapper.appendChild(appNameWrapper);
+    console.log('onclick li', li);
+    logoNameWrapper.onclick = logoNameHandler(item, li, displayContainer);
+
+    return logoNameWrapper;
+  }
+
+  var getLastItemInLi = (isInHistory) => {
+    // let lastItemWrapper;
+    if (isInHistory) {
+      // Create history item
+      let historyWrapper = document.createElement('div');
+      historyWrapper.classList.add('history-item');
+      let historyText = document.createTextNode('remove history');
+      historyWrapper.appendChild(historyText);
+
+      return historyWrapper;
+    } else {
+      // Create block item
+      let blockWrapper = document.createElement('div');
+
+      return blockWrapper;
+    }
+  }
+
+  // Create history li
+  var getHistoryLi = (item, displayContainer) => {
+    let li = document.createElement('li');
+    li.classList.add('list-item');
+
+    // Create logo name wrapper
+    let logoNameWrapper = getLogoNameWrapper(item, li, displayContainer);
+
+    let isInHistory = true;
+    let historyWrapper = getLastItemInLi(isInHistory);
+
+    // Append logo name wrapper & history item to li
+    li.insertBefore(logoNameWrapper, li.firstChild);
+    li.appendChild(historyWrapper);
+
+    return li;
+  }
+
+  var getNormalLi = (item, displayContainer) => {
+    let li = document.createElement('li');
+    li.classList.add('list-item');
+
+    // Create logo name wrapper
+    let logoNameWrapper = getLogoNameWrapper(item, li, displayContainer);
+
+    let isInHistory = false;
+    let historyWrapper = getLastItemInLi(isInHistory);
+
+    // Append logo name wrapper & history item to li
+    li.insertBefore(logoNameWrapper, li.firstChild);
+    li.appendChild(historyWrapper);
+
+    return li;
+  }
+
+  var drawDisplayList = (items, displayContainer) => {
+    items.forEach((item, index) => {
+      if (!item.id) {
+        _this.data.items[index].id = index;
+      }
+
+      // let historyLi = getHistoryLi(item, displayContainer);
+      let normalLi = getNormalLi(item, displayContainer);
+
+      // Append a current li to ul
+      // displayContainer.appendChild(historyLi);
+      displayContainer.appendChild(normalLi);
+    });
+  }
+
+  var addItemToHistory = (selectedLi, displayContainer) => {
+    let clonedLi = selectedLi.cloneNode(true);
+    let isInHistory = true;
+    replaceWith(clonedLi.childNodes[1], getLastItemInLi(isInHistory));
+
+    // Remove it from displayContainer (ul)
+    displayContainer.removeChild(selectedLi);
+
+    // Prepend it to displayContainer (ul)
+    displayContainer.insertBefore(clonedLi, displayContainer.firstChild);
+  }
+
   utils.ApiUtil.get().then(res => {
     this.data = res;
     console.log('Api response', res);
@@ -46,40 +170,8 @@ var handler = (e) => {
     let displayContainer = document.createElement('ul');
     displayContainer.classList.add('display-container');
 
-    this.data.items.forEach(item => {
-      let li = document.createElement('li');
-      li.classList.add('list-item');
-
-      // Create first element, img
-      let appImg = document.createElement('img');
-      appImg.src = item.logo;
-      appImg.classList.add('app-img');
-
-      // Create second element, p
-      let appNameWrapper = document.createElement('p');
-      let appNameText = document.createTextNode(`${item.name}`);
-      appNameWrapper.classList.add('app-name');
-      appNameWrapper.appendChild(appNameText);
-
-      // Create wrapper for logo & name
-      let logoNameWrapper = document.createElement('div');
-      logoNameWrapper.classList.add('logo-name-wrapper');
-      logoNameWrapper.insertBefore(appImg, logoNameWrapper.firstChild);
-      logoNameWrapper.appendChild(appNameWrapper);
-
-      // Create history item
-      let historyWrapper = document.createElement('div');
-      historyWrapper.classList.add('history-item');
-      let historyText = document.createTextNode('remove history');
-      historyWrapper.appendChild(historyText);
-
-      // Append logo name wrapper & history item to li
-      li.insertBefore(logoNameWrapper, li.firstChild);
-      li.appendChild(historyWrapper);
-
-      // Append a current li to ul
-      displayContainer.appendChild(li);
-    });
+    // Draw display list
+    drawDisplayList(this.data.items, displayContainer);
 
     // Put display container after input field
     inputContainer.appendChild(displayContainer);
@@ -89,7 +181,8 @@ var handler = (e) => {
   });
 }
 
-var input = document.getElementById('input');
-input.onclick = handler;
+var input1 = document.getElementById('input1');
+input1.onclick = handler;
 
-
+// var input2 = document.getElementById('input2');
+// input2.onclick = handler;
