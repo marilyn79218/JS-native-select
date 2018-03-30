9970f7e40c3e3ce0bdc230bc97758278b76f6ca4
雷逸志 <ray8863@leiyizhide-MacBook-Pro.local>
2018-03-31 02:49:41 +0800

    Remove unused Function.prototype.clone.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,9 @@ require('./main.css');
 const handlers = require('./handlers/inputFocusHandler.js');
 
 const handler = handlers.handler;
-const nestedHandler = handlers.nestedHandler;
-
-// Add functions to function prototype
-Function.prototype.clone = function() {
-  var that = this;
-  var temp = function temporary() {
-    return that.apply(this, arguments);
-  };
-
-  for(var key in this) {
-      if (this.hasOwnProperty(key)) {
-          temp[key] = this[key];
-      }
-  }
-
-  return temp;
-};
 
 var input1 = document.getElementById('input1');
-input1.onclick = handler.clone();
+input1.onclick = handler;
 
 var input2 = document.getElementById('input2');
-input2.onclick = handler.clone();
+input2.onclick = handler;
